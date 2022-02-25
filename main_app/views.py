a6877b46ef51574b055344ab28e4aaf860f3e578
Max Ma <maxxma97@gmail.com>
2022-02-25 14:01:42 -0500

    set up birds index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Temporary space for Bird Model
+class Bird:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+birds = [
+    Bird('Chip', 'Zebra Finch', 'likes to sing', 2),
+    Bird('Fruit Loop', 'Toucan', 'proud of his beak', 5),
+    Bird('Banana', 'Budgie', 'friendly and sociable', 1),
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def birds_index(request):
+    return render(request, 'birds/index.html', { 'birds': birds })
+
