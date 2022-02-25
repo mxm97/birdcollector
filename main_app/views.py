3fe6b1f9efa953c64af371b6b3b3c8704cbe2d01
Max Ma <maxxma97@gmail.com>
2022-02-25 16:16:14 -0500

    created Bird model

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-# Temporary space for Bird Model
-class Bird:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-birds = [
-    Bird('Chip', 'Zebra Finch', 'likes to sing', 2),
-    Bird('Fruit Loop', 'Toucan', 'proud of his beak', 5),
-    Bird('Banana', 'Budgie', 'friendly and sociable', 1),
-]
+from .models import Bird
 
 # Create your views here.
 def home(request):
@@ -22,5 +9,6 @@ def about(request):
     return render(request, 'about.html')
 
 def birds_index(request):
+    birds = Bird.objects.all()
     return render(request, 'birds/index.html', { 'birds': birds })
 
